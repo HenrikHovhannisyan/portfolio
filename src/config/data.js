e7af8ebbf4cfabf8746d8f54053b4198db5e2331
HenrikHovhannisyan <henrik.hovhannisyan.91@gmail.com>
2023-10-10 16:47:56 +0400

    Added project React-Shop

--- src/config/data.js
+++ src/config/data.js
@@ -1,67 +1,75 @@
-import TO_DO_LIST from "../assets/works/ToDoList.png";
-import PORTFOLIO_IMG from "../assets/works/Portfolio.png";
-import TEXAS_AUTO from "../assets/works/TexasAuto.png";
-import WEATHER from "../assets/works/Weather.png";
-import CAT_CHAT from "../assets/works/CatChat.png";
-import MOVIE_GUIDE from "../assets/works/MovieGuide.png";
-import COUNTRIES from "../assets/works/Countries.png";
-import DELETE_ME from "../assets/works/DeleteMe.png";
+import ToDoList_img from "../assets/works/ToDoList.png";
+import Portfolio_img from "../assets/works/Portfolio.png";
+import TexasAuto_img from "../assets/works/TexasAuto.png";
+import WEATHER_img from "../assets/works/Weather.png";
+import CatChat_img from "../assets/works/CatChat.png";
+import MOVIE_GUIDE_img from "../assets/works/MovieGuide.png";
+import COUNTRIES_img from "../assets/works/Countries.png";
+import DeleteMe_img from "../assets/works/DeleteMe.png";
+import ReactShop_img from "../assets/works/ReactShop.png";
 
 export const PORTFOLIO = [
   {
     id: 1,
     type: "app",
     name: "To Do List",
-    image: TO_DO_LIST,
+    image: ToDoList_img,
     url: "https://react-to-do-list-pi.vercel.app/",
   },
   {
     id: 2,
     type: "web",
     name: "Portfolio",
-    image: PORTFOLIO_IMG,
+    image: Portfolio_img,
     url: "https://portfolio-henrikhovhannisyan.vercel.app/",
   },
   {
     id: 3,
     type: "web",
     name: "Texas Auto",
-    image: TEXAS_AUTO,
+    image: TexasAuto_img,
     url: "https://texas-auto.vercel.app/",
   },
   {
     id: 4,
     type: "app",
     name: "Weather",
-    image: WEATHER,
+    image: WEATHER_img,
     url: "https://weather-app-flax-two-17.vercel.app/",
   },
   {
     id: 5,
     type: "web",
     name: "Cat Chat",
-    image: CAT_CHAT,
+    image: CatChat_img,
     url: "https://cat-chat-silk.vercel.app/",
   },
   {
     id: 6,
     type: "app",
     name: "Movie Guide",
-    image: MOVIE_GUIDE,
+    image: MOVIE_GUIDE_img,
     url: "https://movie-guide-app-gold.vercel.app/",
   },
   {
     id: 7,
     type: "app",
     name: "Countries",
-    image: COUNTRIES,
+    image: COUNTRIES_img,
     url: "https://countries-weld-three.vercel.app/",
   },
   {
     id: 8,
     type: "web",
     name: "Delete Me",
-    image: DELETE_ME,
+    image: DeleteMe_img,
     url: "https://delete-me-green.vercel.app/",
+  },
+  {
+    id: 9,
+    type: "web",
+    name: "React Shop",
+    image: ReactShop_img,
+    url: "https://react-shop-rust-five.vercel.app/",
   },
 ];
